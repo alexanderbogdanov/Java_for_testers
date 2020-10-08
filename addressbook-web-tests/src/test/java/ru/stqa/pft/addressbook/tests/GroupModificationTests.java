5eb0189a726e8a12e1ce61ccb41aed7608e77a48
alexanderbogdanovgit config --global user.name alexanderbogdanovgit config --global user.name alexanderbogdanov <ironweed@list.ru>
2020-10-09 02:04:03 +0300

    Lambda expressions

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -27,6 +27,9 @@ public class GroupModificationTests extends TestBase {
 
         before.remove(before.size() - 1);
         before.add(group);
+
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        app.getSessionHelper().logout();
+
     }
 }
